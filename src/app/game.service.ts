cba5f515c5dee0b7e018238b187babb4efa97403
luciahillrains <lucia.hillrains@aa.com>
2018-02-13 10:32:48 -0600

    removed old menu.  let's get refactoring!

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -4,28 +4,5 @@ import {Kingdom} from './kingdom';
 export class GameService {
 
   constructor() { }
-  NUM_HIRED:number = 5;
-  NUM_LOST:number = 3;
-  NUM_POP_GAIN:number = 500;
-  hireNewClergy(kingdom:Kingdom) {
-  	var newClergy = this.NUM_HIRED;
-  	var deltaKnights = this.NUM_LOST;
-  	kingdom.numClerics = kingdom.numClerics + newClergy;
-  	kingdom.numKnights = kingdom.numKnights - deltaKnights;
-  	return kingdom;
-  }
 
-  hireNewKnights(kingdom:Kingdom) {
-  	var newKnights = this.NUM_HIRED;
-  	var deltaClergy = this.NUM_LOST;
-  	kingdom.numKnights = kingdom.numKnights + newKnights;
-  	kingdom.numClerics = kingdom.numClerics - deltaClergy;
-  	return kingdom;
-  }
-
-  initiateFeast(kingdom:Kingdom) {
-  	var newPop = 500;
-  	kingdom.population = kingdom.population + newPop;
-  	return kingdom;
-  }
 }
